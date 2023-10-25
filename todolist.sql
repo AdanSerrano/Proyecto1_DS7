2d938756d0b7e94d963f1813f7a4420d77e31cd2
AdanSerrano <adan.serrano@utp.ac.pa>
2023-10-25 00:47:10 -0500

    feat(TODO LIST): :hammer: script sql
    update todolist.sql work on local database
    
    finished

--- todolist.sql
+++ todolist.sql
@@ -1,11 +1,17 @@
+-- Tabla de Usuarios
+CREATE TABLE Usuarios (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    NombreUsuario VARCHAR(255) NOT NULL,
+    CorreoElectronico VARCHAR(255) NOT NULL,
+    ContrasenaHash VARCHAR(255) NOT NULL, 
+    FechaRegistro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 -- Tabla de Tipos de Tarea
 CREATE TABLE TiposTarea (
     ID INT PRIMARY KEY AUTO_INCREMENT,
     NombreTipo VARCHAR(255) NOT NULL
 );
-
--- Insertar algunos tipos de tarea por defecto (puedes agregar más según sea necesario)
-INSERT INTO TiposTarea (NombreTipo) VALUES ('Trabajo'), ('Estudio'), ('Personal');
 
 -- Tabla de Tareas
 CREATE TABLE Tareas (
@@ -13,12 +19,25 @@ CREATE TABLE Tareas (
     Titulo VARCHAR(255) NOT NULL,
     Descripcion TEXT,
     Estado ENUM('por hacer', 'en progreso', 'terminada') NOT NULL,
-    FechaCompromiso DATETIME NOT NULL,
+    FechaCompromiso DATE NOT NULL, 
     EtiquetaEditado BOOLEAN NOT NULL,
     Responsable VARCHAR(255) NOT NULL,
     TipoTareaID INT,
-    FOREIGN KEY (TipoTareaID) REFERENCES TiposTarea(ID)
+    UsuarioID INT,
+    FOREIGN KEY (TipoTareaID) REFERENCES TiposTarea(ID),
+    FOREIGN KEY (UsuarioID) REFERENCES Usuarios(ID)
 );
+
+-- CREATE TABLE tareas (
+--     ID INT PRIMARY KEY AUTO_INCREMENT,
+    -- Titulo VARCHAR(255) NOT NULL,
+    -- Descripcion TEXT,
+    -- Estado ENUM('por hacer', 'en progreso', 'terminada') NOT NULL,
+    -- FechaCompromiso DATE NOT NULL, 
+    -- EtiquetaEditado BOOLEAN NOT NULL,
+    -- Responsable VARCHAR(255) NOT NULL,
+    -- TipoTarea INT,
+-- );
 
 
 SELECT TT.NombreTipo, T.Estado, COUNT(*) as TotalTareas
