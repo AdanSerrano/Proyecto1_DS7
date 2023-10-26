b811c5876b43f58a912d6f7869bf45d02a12a2d5
AdanSerrano <adan.serrano@utp.ac.pa>
2023-10-25 19:35:00 -0500

    update label

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php
-  if(array_key_exists('addTask', $_POST))
-  {
-    $miArray = $_POST;
+if (array_key_exists('addTask', $_POST)) {
+  $miArray = $_POST;
 
-    $titulo = $_POST['txtTitle'];
-    $descripcion = $_POST['descripcion'];
-    $estado = $_POST['ddlTaskState'];
-    $fechaCompromiso = $_POST['fechaCompromiso'];
-    $responsable = $_POST['userResponse'];
-    $categoria = $_POST['taskCategorie'];
+  $titulo = $_POST['txtTitle'];
+  $descripcion = $_POST['descripcion'];
+  $estado = $_POST['ddlTaskState'];
+  $fechaCompromiso = $_POST['fechaCompromiso'];
+  $responsable = $_POST['userResponse'];
+  $categoria = $_POST['taskCategorie'];
 
-    require_once("class/Tasks.php");
-      $objTasks = new Tasks();
-      $result = $objTasks->PostTasks($titulo, $descripcion, $estado, $fechaCompromiso, $responsable, $categoria);
-  }
+  require_once("class/Tasks.php");
+  $objTasks = new Tasks();
+  $result = $objTasks->PostTasks($titulo, $descripcion, $estado, $fechaCompromiso, $responsable, $categoria);
+}
 
 ?>
 
@@ -26,24 +25,24 @@
   <title>Checklist App</title>
   <script src="https://cdn.tailwindcss.com"></script>
   <style>
-        .modal {
-            display: none; 
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background-color: rgba(0,0,0,0.5);
-        }
+    .modal {
+      display: none;
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.5);
+    }
 
-        .modal-content {
-            background-color: #fff;
-            width: 80%;
-            max-width: 800px;
-            margin: 100px auto;
-            padding: 20px;
-        }
-    </style>
+    .modal-content {
+      background-color: #fff;
+      width: 80%;
+      max-width: 800px;
+      margin: 100px auto;
+      padding: 20px;
+    }
+  </style>
 
 </head>
 
@@ -53,13 +52,13 @@
   </div>
 
   <div class="flex flex-row">
-    <div class="basis-1/4 rounded-lg">
-      <div class="container px-1">
+    <section class="basis-1/4 rounded-lg">
+      <article class="container px-1">
         <h2 class="font-bold text-xl text-white rounded-lg py-1 text-center bg-blue-500">Crear Tarea</h2>
 
-        <form name="formularioTarea" class="mt-2 p-2 bg-gray-50 rounded-lg"  action="index.php" method="post" >
+        <form name="formularioTarea" class="mt-2 p-2 bg-gray-50 rounded-lg" action="index.php" method="post">
           <label for="txtTitle" class="block text-sm font-medium leading-6 text-gray-900">Titulo</label>
-          <input type="text" name="txtTitle" class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Título"  >
+          <input type="text" name="txtTitle" class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Título">
 
 
           <label for="titulo" class="mt-2 block text-sm font-medium leading-6 text-gray-900">Descripcion</label>
@@ -68,7 +67,7 @@
           <fieldset class="mt-2">
             <legend class="text-sm font-semibold leading-6 text-gray-900">Estado</legend>
             <select name="ddlTaskState" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-            <?php
+              <?php
               require_once("class/Parameter.php");
               $objParameter = new Parameter();
               $listParameter = $objParameter->GetParameter("ESTADOS DE TAREAS");
@@ -76,36 +75,37 @@
                 echo "<option value='" . $parameter['ID_PARAMETER'] . "'>" .  $parameter['PARA_NAME'] . "</option>";
               }
 
-            ?>
-          </select><br>
+              ?>
+            </select><br>
           </fieldset>
 
-          <label for="" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >Fecha de compromiso</label>
-          <input type="datetime-local" name="fechaCompromiso"  /><br />
-          <label for="" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >Responsable</label>
+          <label for="" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Fecha de
+            compromiso</label>
+          <input type="datetime-local" name="fechaCompromiso" /><br />
+          <label for="" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Responsable</label>
           <select name="userResponse" id="userResponse" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
             <option value="" disabled selected>--- Seleccione una ---</option>
             <?php
-              require_once("class/Users.php");
-              $objUsers = new users();
-              $listUser = $objUsers->GetUsers();
-              foreach ($listUser as $user) {
-                echo "<option value='" . $user['ID_USER'] . "'>" . $user['USER_LAST_NAME'] . " " . $user['USER_FIRST_NAME'] . "</option>";
-              }
+            require_once("class/Users.php");
+            $objUsers = new users();
+            $listUser = $objUsers->GetUsers();
+            foreach ($listUser as $user) {
+              echo "<option value='" . $user['ID_USER'] . "'>" . $user['USER_LAST_NAME'] . " " . $user['USER_FIRST_NAME'] . "</option>";
+            }
 
             ?>
           </select><br>
 
-          <label for="" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >Categorias</label>
-          <select name="taskCategorie" id="taskCategorie" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline" >
+          <label for="" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Categorias</label>
+          <select name="taskCategorie" id="taskCategorie" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
             <option value="" disabled selected>--- Seleccione una ---</option>
             <?php
-              require_once("class/Categorie.php");
-              $objCatego = new Categorie();
-              $listCatego = $objCatego->GetCategorie();
-              foreach ($listCatego as $Catego) {
-                echo "<option value='" . $Catego['ID_CATEGORIE'] . "'>" . $Catego['CAT_NAME'] ."</option>";
-              }
+            require_once("class/Categorie.php");
+            $objCatego = new Categorie();
+            $listCatego = $objCatego->GetCategorie();
+            foreach ($listCatego as $Catego) {
+              echo "<option value='" . $Catego['ID_CATEGORIE'] . "'>" . $Catego['CAT_NAME'] . "</option>";
+            }
 
             ?>
           </select><br>
@@ -114,55 +114,64 @@
 
         </form>
         <div id="lista-tareas"></div>
-      </div>
-    </div>
+      </article>
+    </section>
 
-    <div class="basis-1/4 rounded-lg">
+    <article class="basis-1/4 rounded-lg">
       <div class="container px-1">
         <h2 class="font-bold text-xl text-white rounded-lg py-1 text-center bg-red-500">Por Hacer</h2>
         <div class="mt-2 p-2 bg-gray-50 rounded-lg">
           <?php
-             require_once("class/Tasks.php");
-             $objTasks = new Tasks();
-             $listTasks = $objTasks->GetTasks();
-             foreach ($listTasks as $tasks) {
-                if($tasks['TASK_STATE'] == 1){
-                  echo "<div>";
-                  echo "<label><b>" . $tasks['TASK_NAME'] ."</b></label><br>";
-                  echo "<label><b>Descrición: </b>" . $tasks['TASK_DESCRIPTION'] ."</label><br>";
-                  echo "<label><b>Categorias: </b>" . $tasks['CAT_NAME'] ."</label><br>";
-                  echo "<label><b>Fecha de compromiso: </b>" . $tasks['TASK_DATE'] ."</label><br>";            
-                  echo "<label><b>Responsable: </b>" . $tasks['USER_NAME'] ."</label><br>";            
-                  echo "</div>";     
-                  echo "<button>Modificar</button>";
-                }
-             }
+          require_once("class/Tasks.php");
+          $objTasks = new Tasks();
+          $listTasks = $objTasks->GetTasks();
+          $style = "class='mt-4 text-sm text-gray-600'";
+          foreach ($listTasks as $tasks) {
+            if ($tasks['TASK_STATE'] == 1) {
+              echo "<div class='flex flex-col bg-white rounded-lg shadow-lg overflow-hidden bg-black m-2'>";
+              echo "<div class='p-2'>";
+              echo "<label class='text-base font-semibold text-gray-800'><b>" . $tasks['TASK_NAME'] . "</b></label><br>";
+              echo "<label $style><b>Descrición: </b>" . $tasks['TASK_DESCRIPTION'] . "</label><br>";
+              echo "<label $style><b>Categorias: </b>" . $tasks['CAT_NAME'] . "</label><br>";
+              echo "<span class='text-base font-semibold text-gray-600 uppercase tracking-wide center' >" . $tasks['USER_NAME'] . "</span>";
+              echo "</div>";
+              echo "<div class='flex items-center justify-around px-2 py-2 bg-gray-100'>";
+              echo "<button class='px-3 py-1 text-xs font-semibold text-gray-700 uppercase bg-gray-200 rounded-full'>Editar</button>";
+              echo "<button class='px-3 py-1 text-xs font-semibold text-gray-700 uppercase bg-gray-200 rounded-full bg-red-400'>eliminar</button>";
+              echo "</div>";
+              echo "</div>";
+            }
+          }
 
           ?>
         </div>
       </div>
-    </div>
+    </article>
 
-    <div class="basis-1/4 rounded-lg">
+    <article class="basis-1/4 rounded-lg">
       <div class="container px-1">
         <h2 class="font-bold text-xl text-white rounded-lg py-1 text-center bg-amber-500">En Curso</h2>
-      </div>
-    </div>
 
-    <div class="basis-1/4 rounded-lg">
+      </div>
+    </article>
+
+    <article class="basis-1/4 rounded-lg">
       <div class="container px-1">
         <h2 class="font-bold text-xl text-white rounded-lg py-1 text-center bg-emerald-500">Terminada</h2>
       </div>
-    </div>
+    </article>
   </div>
 
   <div>
     <h3>Filtro por categorias</h3>
     <?php
-      foreach ($listCatego as $Catego) {
-        echo "<input type='checkbox' id='ckCategories' name='ckCategories' value='" . $Catego['ID_CATEGORIE'] ."' checked>\n";
-        echo "<label for='ckCategories'>" . $Catego['CAT_NAME'] ."</label><br>";
-      }
+    require_once("class/Categorie.php");
+    $objCatego = new Categorie();
+    $listCatego = $objCatego->GetCategorie();
+    foreach ($listCatego as $Catego) {
+      echo "<input type='checkbox' id='ckCategories' name='ckCategories' value='" . $Catego['ID_CATEGORIE'] . "' checked>\n";
+      echo "<label for='ckCategories'>" . $Catego['CAT_NAME'] . "</label><br>";
+    }
     ?>
     <button>Filtrar categorias</button>
     <button>Agregar categorias</button>
@@ -171,61 +180,83 @@
   <div>
     <h3>Filtro por Usuarios</h3>
     <?php
-      foreach ($listUser as $user) {
-        echo "<input type='checkbox' id='ckUser' name='ckUser' value='" . $user['ID_USER'] ."' checked>\n";
-        echo "<label for='ckUser'>" . $user['USER_LAST_NAME'] . " " . $user['USER_FIRST_NAME'] . "</label><br>";
-      }
+    require_once("class/Users.php");
+    $objUsers = new users();
+    $listUser = $objUsers->GetUsers();
+    foreach ($listUser as $user) {
+      echo "<input type='checkbox' id='ckUser' name='ckUser' value='" . $user['ID_USER'] . "' checked>\n";
+      echo "<label for='ckUser'>" . $user['USER_LAST_NAME'] . " " . $user['USER_FIRST_NAME'] . "</label><br>";
+    }
     ?>
     <button>Filtrar Usuarios</button>
     <button>Agregar Usuarios</button>
   </div>
 
-  <button data-modal-target="defaultModal" data-modal-toggle="defaultModal"  id="abrirModal">Abrir Modal</button>
+  <button data-modal-target="defaultModal" data-modal-toggle="defaultModal" id="abrirModal">Abrir Modal</button>
 
-    <!-- Modal -->
-    <div id="miModal" class="modal">
-      <div class="modal-content">
-        <div class="flex items-start justify-between p-4 border-b rounded-t">
-          <h3 class="text-xl font-semibold text-gray-900 ">Modificar tarea</h3>
-          <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
-            <span id="cerrarModal" style="cursor: pointer;">&times;</span>    
-            <span class="sr-only">Close modal</span>
-          </button>
-        </div>
-        <div class="p-6 space-y-6">
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-            </p>
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-            </p>
-        </div>
+  <!-- Modal -->
+  <section id="miModal" class="modal">
+    <div class="modal-content">
+      <div class="flex items-start justify-between p-4 border-b rounded-t">
+        <h3 class="text-xl font-semibold text-gray-900 ">Modificar tarea</h3>
+        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
+          <span id="cerrarModal" style="cursor: pointer;">&times;</span>
+          <span class="sr-only">Close modal</span>
+        </button>
       </div>
+      <div class="p-6 space-y-6">
+        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+          With less than a month to go before the European Union enacts new consumer privacy laws for its
+          citizens, companies around the world are updating their terms of service agreements to comply.
+        </p>
+        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+          The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is
+          meant to ensure a common set of data rights in the European Union. It requires organizations to
+          notify users as soon as possible of high-risk data breaches that could personally affect them.
+        </p>
+      </div>
+    </div>
+  </section>
+
+
+
   </div>
-
-
-
-</div>
 
   <script src="script.js"></script>
   <script>
     document.addEventListener('DOMContentLoaded', function() {});
 
     var botonAbrir = document.getElementById("abrirModal");
-        var modal = document.getElementById("miModal");
-        var botonCerrar = document.getElementById("cerrarModal");
+    var modal = document.getElementById("miModal");
+    var botonCerrar = document.getElementById("cerrarModal");
 
-        botonAbrir.onclick = function() {
-            modal.style.display = "block"; 
-        }
+    botonAbrir.onclick = function() {
+      modal.style.display = "block";
+    }
 
-        botonCerrar.onclick = function() {
-            modal.style.display = "none";
-        }
+    botonCerrar.onclick = function() {
+      modal.style.display = "none";
+    }
   </script>
 </body>
 
 </html>
 
 
+<?php
+if (array_key_exists('addTask', $_POST)) {
+  $miArray = $_POST;
 
+  $titulo = $_POST['txtTitle'];
+  $descripcion = $_POST['descripcion'];
+  $estado = $_POST['ddlTaskState'];
+  $fechaCompromiso = $_POST['fechaCompromiso'];
+  $responsable = $_POST['userResponse'];
+  $categoria = $_POST['taskCategorie'];
+
+  require_once("class/Tasks.php");
+  $objTasks = new Tasks();
+  $result = $objTasks->PostTasks($titulo, $descripcion, $estado, $fechaCompromiso, $responsable, $categoria);
+}
+
+?>
